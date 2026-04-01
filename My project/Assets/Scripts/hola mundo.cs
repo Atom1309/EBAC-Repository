684766841d8bb89e498d997993696b68b39514fe
Atom1309 <gecko830913@gmail.com>
2026-03-31 20:05:45 -0400

    script
    manejo de debug, log, error, warning y print

---
+++ My project/Assets/Scripts/hola mundo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class holamundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("algo pasa");
+        Debug.LogWarning("algo va a pasar");
+        Debug.LogError("algo esta muy muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
