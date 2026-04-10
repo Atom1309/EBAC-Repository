2ab9a44dac7a8dda8c1a321770c01e5070715237
Atom1309 <gecko830913@gmail.com>
2026-04-09 20:06:43 -0400

    creacion de figura
    se agrgan componentes de manera manual

---
+++ My project/Assets/Scripts/componente3.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class componente3 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        componente1.miObjeto.name = "nuevoNombreDeMiObjeto";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- My project/Assets/Scripts/hola mundo.cs
+++ My project/Assets/Scripts/hola mundo.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class holamundo : MonoBehaviour
@@ -5,18 +6,35 @@ public class holamundo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("algo paso");
-        Debug.Log("algo pasa");
+        
         Debug.LogWarning("algo va a pasar");
         Debug.LogError("algo esta muy muy mal");
-        Debug.Log("algo nuevo");
-        Debug.Log("prueba2");
+       
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        // = x + 1;
+        //ebug.Log(x);
+
+        Debug.Log("hola desde update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("hola desde fixed update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("hola desde late update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto ha sido habilitado");
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto a sido inhabilitado");
     }
 }
